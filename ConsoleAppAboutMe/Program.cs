c7ec482a55efb142cb5152b8c8a97cab9c28e463
Darya_KRV <darya-korovataya@yandex.ru>
2023-03-06 17:50:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppAboutMe/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleAppAboutMe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string MyName = "Darya";
+            int Age = 41;
+            bool Pet = true;
+            double Size = 39.0;
+            Console.WriteLine("My name is {0}\nMy Age is {1}\nDo I have a pet? {2}\nMy shoe size is {3}", MyName, Age, Pet, Size);
+        }
+    }
+}
